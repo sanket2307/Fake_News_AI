9a62cce1e66f43af231c6c8f7dbc4fc06825b7c3
sanket2307 <sanket.phafat@gmail.com>
2020-10-17 12:18:40 +0530

    add Dataset

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
-# This is a sample Python script.
+from wordcloud import WordCloud, STOPWORDS
+import matplotlib.pyplot as plt
+from sklearn import metrics
+from sklearn.metrics import confusion_matrix
+import seaborn as sns
+import numpy as np
+import pandas as pd
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import os
+import re
+import nltk
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+train = pd.read_csv('train.csv')
+test = pd.read_csv('test.csv')
